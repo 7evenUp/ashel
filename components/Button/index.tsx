a7d02b1fe878351adc655d97a9bb3895544b301f
Artyom <sheludeshev.artyom@mail.ru>
2022-05-31 01:34:17 +0300

    Made some responsibility

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -2,12 +2,14 @@ import styles from './Button.module.css'
 
 type ButtonProps = {
   title: string
+  onClick: () => void
   large?: boolean
 }
 
-const Button = ({title, large}: ButtonProps) => {
+const Button = ({title, large, onClick}: ButtonProps) => {
   return (
     <button
+      onClick={onClick}
       className={`${styles.button} ${large && styles.button__large}`}>
         {title}
     </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import Button from '../components/Button'
+import Card from '../components/Card'
 import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
@@ -13,17 +15,18 @@ const Home: NextPage = () => {
         <meta name="description" content="Ashel website blog-portfolio" />
       </Head>
       <Header />
-
       <main className={styles.main}>
         <div className={styles.welcome}>
           <div className={styles.welcome_text}>
             <h2>ты зашёл сюда, чтобы получше меня узнать? Ну тогда я не против</h2>
             <div className={styles.cta}>
               <Image src="/arrow.svg" alt="Arrow image" width={178} height={106} />
-              <Button title="Тыкни сюда" />
+              <Button title="Тыкни сюда" large onClick={() => alert("Ты лох")} />
             </div>
           </div>
-          <Image className={styles.welcome_image} src="/welcome.svg" alt="Welcome image" width={380} height={334} />
+          <div className={styles.welcome_image}>
+            <Image src="/welcome.svg" alt="Welcome image" width={380} height={334} />
+          </div>
         </div>
         <div className={styles.info}>
           <div className={styles.info_text}>
@@ -34,7 +37,19 @@ const Home: NextPage = () => {
           </div>
           <span className={styles.separator} />
           <div className={styles.bio}>
-            1
+            <Card title="Программист" accentColor="red">
+              <p>
+                Я пишу клиентскую часть на React, серверную на Node.js. Могу немного Python. Для разработки мобильных приложений использую React Native. Также практикую себя в UI/UX дизайне
+              </p>
+              <Link href="/works">
+                <a>Посмотреть работы</a>
+              </Link>
+            </Card>
+            <Card title='Футболист' accentColor="green">
+              <p>
+                Играю в футбол с пелёнок. В 2017 переехал в Санкт-Петербург и начал играть в футбол 11х11. В данный момент времени играю в ПФЛ (Пивной футбольной лиге). Очень много тренируюсь и пропагандирую ЗОЖ
+              </p>
+            </Card>
           </div>
         </div>
       </main>
