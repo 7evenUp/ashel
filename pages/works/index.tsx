972e1cc1d3b3188ba1b948ca0d2d1aa0d434ccab
Artyom <sheludeshev.artyom@mail.ru>
2022-06-24 14:00:28 +0300

    Changed works data

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -8,21 +8,11 @@ const Works = () => {
         title='Веб-сайт'
         subtitle='Мой личный веб-сайт'
         description='Я разработал данный проект для личных целей: чтобы моё лицо тусовалось на медиа пространствах'
-        stack={['React', 'Next.js', 'Figma', 'Firebase']}
+        stack={['Next.js', 'Typescript', 'Firebase']}
         imgSrc='/works/ashel_preview.png'
         githubLink='https://github.com/7evenUp/ashel'
         previewLink='https://ashel.vercel.app/'
         accentColor='cyan' />
-
-      <WorkCard
-        title='Мобильное приложение'
-        subtitle='Мобильное приложение для тренировок по футболу'
-        description='Я разработал данное приложение с целью упрощения собственного тренировочного процесса. Идея пришла в голову еще на 1 курсе моего обучения. Однако тогда я знаниями мобильной разработки не располагал и не мог реализовать проект.'
-        stack={['React', 'React Native', 'Redux', 'Figma', 'Firebase']}
-        imgSrc='/works/ashel_preview.png'
-        // githubLink='https://github.com/7evenUp/ashel'
-        previewLink='https://ashel.vercel.app/'
-        accentColor='green' />
     </main>
   )
 }
