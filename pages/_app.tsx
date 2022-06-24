6495a1545b39a9d5c3f621e46d56c108dcdcacb4
Artyom <sheludeshev.artyom@mail.ru>
2022-06-24 19:40:11 +0300

    Added Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,27 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import Head from 'next/head'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive">
+        {
+          `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${process.env.GOOGLE_ANALYTICS_ID});
+          `
+        }
+      </Script>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
       </Head>
