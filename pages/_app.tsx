51bfd75fd2a9e46d1912e6bf8e7a2a19d2117245
Artyom <sheludeshev.artyom@mail.ru>
2022-07-06 15:03:46 +0300

    Fixed ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,19 +9,15 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Script
         strategy='afterInteractive'
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`} />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive">
-        {
-          `
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`} />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', ${process.env.GOOGLE_ANALYTICS_ID});
-          `
-        }
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+        `}
       </Script>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
